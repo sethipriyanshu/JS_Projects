61ad1cbc4537a266dfdda1b610752f0e722bb1c7
Priyanshu <sethipriyanshu@outlook.com>
2023-12-30 14:31:17 -0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function getComputerChoice(){
+    const array =["rock","paper","scissors"];
+    const selection = array[Math.floor(Math.random()*3)];
+    return selection;
+}
+console.log(getComputerChoice());
